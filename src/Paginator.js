94106d61363f8ffb0c7a65846dfdfd30d971f3b2
eoin.trainor <eoin.trainor@paconsulting.com>
2022-11-23 17:27:52 +0000

    Pagination added to results

--- src/Paginator.js
+++ src/Paginator.js
@@ -1,10 +1,63 @@
-// import { ReactPaginate } from "react-paginate";
+import ReactPaginate from "react-paginate";
 
-const Paginator = ({ resultsNumber }) => {
+import { useState } from "react";
+import "./Paginator.css";
+
+const Paginator = ({ pagesNeeded, pets, animal, resultsNumber, setPets }) => {
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const endOffset = itemOffset + 10;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+
+  const pageCount = pagesNeeded;
+
+  console.log(pets);
+
+  async function GetPets(pageNumber) {
+    console.log("page number requested: " + pageNumber);
+    console.log("animal is: " + animal);
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&page=${pageNumber}`
+    );
+    const json = await res.json();
+    console.log(json);
+    setPets(json.pets);
+  }
+
+  const handlePageClick = (event) => {
+    GetPets(event.selected);
+    console.log("event:" + event);
+    const newOffset = (event.selected * 10) % resultsNumber;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div>
-      <p>Result number: {resultsNumber}</p>
-    </div>
+    <>
+      <p>{pagesNeeded}</p>
+      <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={9}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
+    </>
   );
 };
 
